8cbfcdcc8318b00753aef9a214d7417abc958895
Igor Marchuk <78100460+Hutsul-coder@users.noreply.github.com>
2021-10-20 11:44:48 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const cat = {
+  "name" : "Tom", // "name" - it`s a key, "Tom" - value
+  "color" : "blue",
+  "legs" : 4,
+  eyes : "red" // key may be without quotes, but space can`t be between words
+  }
+  
